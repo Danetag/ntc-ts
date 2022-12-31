5953288f73c2843ccf216e317dca0ed23314a648
Arnaud Tanielian <arno.tanielian@gmail.com>
2022-12-30 20:09:42 -0500

    fix: Add forgotten exports in main index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import { flushCachedColors, getColorName, initColors } from './ntc';
+import {
+    cachedColors,
+    colors,
+    flushCachedColors,
+    getColorName,
+    initColors,
+    NOT_A_COLOR
+} from './ntc';
 import { MINIMAL_COLORS } from './colors/minimal';
 import { ORIGINAL_COLORS } from './colors/original';
 
 export {
+    cachedColors,
+    colors,
     flushCachedColors,
     getColorName,
     initColors,
     MINIMAL_COLORS,
+    NOT_A_COLOR,
     ORIGINAL_COLORS
 };
